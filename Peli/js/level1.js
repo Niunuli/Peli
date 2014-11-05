35caed4e0c4e904fd994e09e0db7ce47e9d645c4
Niunuli <niinaperala@gmail.com>
2014-11-05 19:48:25 +0200

    perus funcit ilman sisältöä

--- Peli/js/level1.js
+++ Peli/js/level1.js
@@ -1,1 +1,40 @@
-sd
+var FrogAdventures = FrogAdventures || {};
+
+//title screen
+FrogAdventures.level1 = function(){
+
+	var player;
+	var ground;
+	var platforms;
+	var cursors;
+
+	var flies;
+	var score = 0;
+	var scoreText;
+};
+
+FrogAdventures.level1.prototype = {
+	
+	create: function() {
+  
+  
+	},
+  
+  
+	update: function() {
+	
+	},
+	
+	
+	collectfly: function  (player, fly) {
+    
+    // Removes the fly from the screen
+    fly.kill();
+
+	//  Add and update the score
+    score += 10;
+    scoreText.text = 'Score: ' + score;
+
+}
+	
+	};
